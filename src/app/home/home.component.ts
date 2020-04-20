3fb6c085dc53ce90275ca60ec88f69930b17ecdc
Dureksha Wasala <durekshawasala@gmail.com>
2020-04-20 08:38:12 +0530

    Sorting Function Improved

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Event} from '../../event';
 
 @Component({
@@ -7,9 +7,9 @@ import {Event} from '../../event';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  event = new  Event('0', 'Event 1', new Date('2020-04-21'), {hour: 15, minute: 30}, {hour: 16, minute: 30});
-  event2 = new  Event('1', 'Event 2', new Date('2020-04-21'), {hour: 22, minute: 30}, {hour: 23, minute: 30});
-  event3 = new  Event('2', 'Event 3', new Date('2020-04-19'), {hour: 15, minute: 40}, {hour: 15, minute: 50});
+  event = new  Event('0', 'Event 1', new Date('2020-04-25'), {hour: 15, minute: 30}, {hour: 16, minute: 30});
+  event2 = new  Event('1', 'Event 2', new Date('2020-04-26'), {hour: 22, minute: 30}, {hour: 23, minute: 30});
+  event3 = new  Event('2', 'Event 3', new Date('2020-04-20'), {hour: 15, minute: 40}, {hour: 15, minute: 50});
   eventList = [];
   eventsInADay: Event[];
   selectedDate =  new Date();
@@ -18,28 +18,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.eventList.push(this.event, this.event2, this.event3);
-    this.sortEventList();
-    this.nextEvent = this.eventList[0];
-    // tslint:disable-next-line:prefer-const
-    let events = [];
-    for (const event of this.eventList){
-      if (event.day.toDateString() === this.selectedDate.toDateString()){
-        events.push(event);
-      }
-    }
-    this.eventsInADay = events;
+    this.updateData();
   }
 
   receiveDate($event) {
     this.selectedDate = $event;
-    // tslint:disable-next-line:prefer-const
-    let events = [];
-    for (const event of this.eventList){
-      if (event.day.toDateString() === this.selectedDate.toDateString()){
-        events.push(event);
-      }
-    }
-    this.eventsInADay = events;
+    this.updateData();
   }
 
   receiveNewEvent($event){
@@ -48,29 +32,44 @@ export class HomeComponent implements OnInit {
   }
 
   deleteEvent($event){
-    // tslint:disable-next-line:prefer-const
     this.eventList.splice( this.eventList.indexOf($event), 1 );
-    this.sortEventList();
-    console.log(this.eventList);
-    this.nextEvent = this.eventList[0];
-    // tslint:disable-next-line:prefer-const
-    let events = [];
-    for (const event of this.eventList){
-      if (event.day.toDateString() === this.selectedDate.toDateString()){
-        events.push(event);
-      }
-    }
-    this.eventsInADay = events;
+    this.updateData();
   }
 
   updateEvent($event){
     console.log('Home Update Event');
     const index = this.eventList.findIndex(item => item.id === $event.id);
     this.eventList.splice(index, 1, $event);
+    this.updateData();
+  }
 
+  sortEventList(){
+    // tslint:disable-next-line:prefer-const only-arrow-functions
+    this.eventList = this.eventList.sort(function(a, b) {
+      if (a.day < b.day) {
+        return -1;
+      }
+      if (a.day > b.day) {
+        return 1;
+      }
+      if (a.startTime.hour > b.startTime.hour) {
+        return 1;
+      }
+      if (a.startTime.hour < b.startTime.hour) {
+        return -1;
+      }
+      if (a.startTime.minute > b.startTime.minute) {
+        return 1;
+      }
+      if (a.startTime.minute < b.startTime.minute) {
+        return -1;
+      }
+    });
+
+  }
+
+  updateData(){
     this.sortEventList();
-    console.log(this.eventList);
-    this.nextEvent = this.eventList[0];
     // tslint:disable-next-line:prefer-const
     let events = [];
     for (const event of this.eventList){
@@ -79,14 +78,6 @@ export class HomeComponent implements OnInit {
       }
     }
     this.eventsInADay = events;
-  }
-
-  sortEventList(){
-    // tslint:disable-next-line:only-arrow-functions
-    const list = this.eventList.sort(function(a, b){
-      return a.day - b.day;
-    });
-    this.eventList = list;
     this.nextEvent = this.eventList[0];
   }
 
